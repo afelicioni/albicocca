2ea0bc5856721d0c9a8ab1032b8728f194171c55
Alessio Felicioni <afelicioni@users.noreply.github.com>
2019-12-28 19:39:26 +0100

    Posizionamento pdf con campi variabili da JSON sopra un file esistente

--- index.php
+++ index.php
@@ -1,22 +1,31 @@
 <?php
 require __DIR__ . '/vendor/autoload.php';
+require_once(__DIR__ . '/vendor/tecnickcom/tcpdf/tcpdf.php');
 
 use mikehaertl\pdftk\Pdf;
 
-//use mikehaertl\shellcommand\Command;
-//
-//
-//$command = new Command('/usr/bin/pdftk');
-//if ($command->execute()) {
-//	echo $command->getOutput();
-//} else {
-//	echo $command->getError();
-//	$exitCode = $command->getExitCodes();
-//}
+$sPercorsoPDFBase = __DIR__ . '/input/anagrafica.pdf';
+$sPercorsoPDFDatiVariabili = __DIR__ . '/output/dativariabili.pdf';
+$sPercorsoPDFFinale = __DIR__ . '/output/output.pdf';
+$oCliente = json_decode(file_get_contents(__DIR__ . '/input/cliente.json'));
+
+$pdf = new \TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
+$pdf->setPrintHeader(false);
+$pdf->setPrintFooter(false);
+$pdf->SetMargins(0, 0, 0);
+$pdf->AddPage();
+foreach ($oCliente as $dato) {
+	$pdf->SetXY($dato->posizione->x, $dato->posizione->y);
+	$pdf->Write(1, $dato->valore);
+}
+$pdf->Output($sPercorsoPDFDatiVariabili, 'F');
 
 
 
-$pdf = new Pdf(__DIR__ . '/input/pedaggio.pdf');
-$pdf->burst(__DIR__ . '/output/page_%d.pdf');
+$pdf = new Pdf($sPercorsoPDFBase);
+$pdf->stamp($sPercorsoPDFDatiVariabili)
+	->compress()
+	->saveAs($sPercorsoPDFFinale);
 
+unlink($sPercorsoPDFDatiVariabili);
 echo 'finito';
