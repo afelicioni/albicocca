d8ab988fbc67e48fe75c7732be2f2d8ce1ae6fe7
Alessio Felicioni <afelicioni@users.noreply.github.com>
2019-12-28 13:41:50 +0100

    Aggiunge composer, esempio uso classe php-pdftk per divisione documento multipagina in file da singole pagine

--- index.php
+++ index.php
@@ -1,5 +1,22 @@
 <?php
+require __DIR__ . '/vendor/autoload.php';
 
-$sToolPath = '/usr/bin/pdftk 2>&1';
-$last = exec($sToolPath, $aOutput, $nReturn);
-print_r(array($aOutput, $nReturn, $last));
+use mikehaertl\pdftk\Pdf;
+
+//use mikehaertl\shellcommand\Command;
+//
+//
+//$command = new Command('/usr/bin/pdftk');
+//if ($command->execute()) {
+//	echo $command->getOutput();
+//} else {
+//	echo $command->getError();
+//	$exitCode = $command->getExitCodes();
+//}
+
+
+
+$pdf = new Pdf(__DIR__ . '/input/pedaggio.pdf');
+$pdf->burst(__DIR__ . '/output/page_%d.pdf');
+
+echo 'finito';
